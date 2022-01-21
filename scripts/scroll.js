dd682813d91e49f66f5b2c7295ea20d7727d819f
Kromus22 <kromus2@gmail.com>
2022-01-21 18:23:44 +0500

    first commit

---
+++ scripts/scroll.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll('.header-menu__item a')
+const link = document.querySelector('.card-details__link-characteristics')
+
+seamless.polyfill()
+
+links.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const id = element.getAttribute('href').substring(1)
+    const section = document.getElementById(id)
+
+    if (section) {
+      seamless.elementScrollIntoView(section, {
+        behavior: 'smooth',
+        block: 'start'
+      })
+    } else {
+      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+      });
+    }
+  })
+})
+
